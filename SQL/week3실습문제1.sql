16067cfb27f9c0e85bad0fa7c25b0695038cc9d2
SHJo <ibloger14@naver.com>
2022-01-06 21:44:29 +0900

    Update week3실습문제1.sql

--- SQL/week3실습문제1.sql
+++ SQL/week3실습문제1.sql
@@ -1,1 +1,9 @@
+--- 문제1번) 매출을 가장 많이 올린 dvd 고객 이름은? (subquery 활용)
 
+select c.first_name, c.last_name 
+from customer c
+where c.customer_id in (select customer_id
+from payment p 
+group by customer_id
+order by sum(amount) desc
+limit 1)

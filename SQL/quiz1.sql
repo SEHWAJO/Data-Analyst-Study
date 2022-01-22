eec8f57752277a5ba468e14d9eccef7f5fe2b5f0
SHJo <ibloger14@naver.com>
2022-01-22 13:49:40 +0900

    Update quiz1.sql

--- SQL/quiz1.sql
+++ SQL/quiz1.sql
@@ -8,3 +8,8 @@ from products
 select ordernumber, orderdate, customerid 
 from orders 
 order by orderdate desc
+
+--문제4번) customers 테이블을 이용하여,  고객의 이름과 성을 하나의 컬럼으로 전체 이름을 보여주세요 (이름, 성 의 형태로  full_name 이라는 이름으로 보여주세요)
+
+select first_name ||', '|| last_name  as  full_name
+from customer
